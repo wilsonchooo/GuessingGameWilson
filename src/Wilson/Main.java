5bd2892f0c40e08c41040c010185849e9c5b432f
Wilson Cho <wcho1205@bths.edu>
2018-09-25 11:25:32 -0400

    Initial commit

---
+++ src/Wilson/Main.java
@@ -0,0 +1,30 @@
+package Wilson;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean correct = false;
+        int randomnum = (int)(10.0*Math.random());
+        Scanner scan = new Scanner(System.in);
+        //System.out.println(randomnum);
+
+        while (correct == false);
+        {
+            int number = scan.nextInt();
+            if (number != randomnum) {
+                System.out.println("wrong");
+            }
+            if (number == randomnum)
+            {
+                correct =true;
+            }
+        }
+        if (correct ==true)
+        {
+            System.out.println("wrong");
+
+        }
+    }
+}
