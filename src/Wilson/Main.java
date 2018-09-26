3c32a532690341cb552582e0d544601cbd7bf004
wilsonchooo <43213133+wilsonchooo@users.noreply.github.com>
2018-09-26 00:01:48 -0400

    Add files via upload

--- src/Wilson/Main.java
+++ src/Wilson/Main.java
@@ -6,32 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         boolean correct = false;
-        int randomnum = (int)(10.0*Math.random());
+        int randomnum = (int) (9.0 * Math.random()+1);
         Scanner scan = new Scanner(System.in);
 
-        while (correct = false);
-        {
-            int number = scan.nextInt();
+        int number =0;
+
+
+        while (number != randomnum) {
+            number = scan.nextInt();
 
 
             if (number != randomnum) {
                 System.out.println("wrong");
-                 number = scan.nextInt();
 
             }
 
-            if (number == randomnum)
-            {
+            if (number == randomnum) {
                 correct = true;
                 System.out.println("correct");
 
             }
-
-        }
-        if (correct == true)
-        {
-            System.out.println("correct");
-
         }
     }
 }//eeeeeeeeeeeeeee
