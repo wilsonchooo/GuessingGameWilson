a18541d61a8e020544256a61de373d1f4ee2dce1
wilsonchooo <wcho1205@bths.edu>
2018-09-26 17:19:46 -0400

    guessing game

--- src/Wilson/objective2.java
+++ src/Wilson/objective2.java
@@ -8,34 +8,54 @@ public class objective2 {
         boolean correct = false;
         int min = 1;
         int max = 100;
-
+        int guesses = 0;
         int number = (int) (max * Math.random()+1);
         Scanner scan = new Scanner(System.in);
-        System.out.println("is your number" + number);
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+
+        System.out.println("is your number " + number +", " + name +"?");
 
 
         while (correct == false) {
             String str = scan.nextLine();
 
             if (str.equals("correct")) {
-                System.out.println("WE GOT A WINNER");
+                System.out.println("WE GOT A WINNER!");
+                System.out.println("It took you "+guesses + " tries!");
+
                 correct = true;
             }
 
             if (str.equals("higher")) {
-                min = number;
+                min = number + 1;
                  number = (int) (Math.random()*(max - min))+min;
+                if (number<= min||number>= max )
+                {
+                    number = (int) (Math.random()*(max - min))+min;
+
+                }
+                guesses++;
                 System.out.println(min);
+                System.out.println(max);
 
                 // number = (int) (Math.random() * (100 - number) + 1) + number;
-                System.out.println("is your number" + number);
+                System.out.println("is your number " + number +", " + name +"?");
             }
 
             if (str.equals("lower")) {
-                max = number;
+                max = number-1;
                  number = (int) (Math.random()*(max - min))+min;
-                //number = (int) (Math.random() * (number - 100) + 1) + number;
-                System.out.println("is your number" + number);
+                 if (number<= min||number>= max )
+                 {
+                     number = (int) (Math.random()*(max - min))+min;
+                 }
+                 guesses++;
+                System.out.println(min);
+
+                System.out.println(max);
+
+                System.out.println("is your number" + number +", " + name +"?");
 
 
             }
@@ -49,10 +69,3 @@ public class objective2 {
 }
 
 
-/*
-        int randomWithRange(int min, int max)
-        {
-        int range = (max - min) + 1;
-        return (int)(Math.random() * range) + min;
-        }
-        */
